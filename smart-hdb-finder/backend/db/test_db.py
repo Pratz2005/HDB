ed366401c871c0af331542f09080694b0bec2b2c
nursharlynn <80143012+nursharlynn@users.noreply.github.com>
2025-02-27 10:04:06 +0800

    Added requirements.txt, moved database connection tobackend

---
+++ smart-hdb-finder/backend/db/test_db.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+# Load environment variables
+load_dotenv()
+
+# Get MongoDB URI from .env.local
+MONGODB_URI = os.getenv("MONGODB_URI")
+print(MONGODB_URI)
+
+# Test connection
+try:
+    client = MongoClient(MONGODB_URI)
+    db = client.admin  # Test connection with the admin database
+    print("✅ Connected to MongoDB successfully!")
+    #print("Databases:", client.list_database_names())  # List databases
+except Exception as e:
+    print("❌ MongoDB connection failed:", e)
