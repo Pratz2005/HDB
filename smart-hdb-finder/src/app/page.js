469330edc73d4e2aff5646f2134a71896e835154
Pratz2005 <prathammehra24457@gmail.com>
2025-04-16 16:16:16 +0800

    remove shadcn

--- smart-hdb-finder/src/app/page.js
+++ smart-hdb-finder/src/app/page.js
@@ -1,6 +1,5 @@
 // app/page.js
 "use client";
-import { buttonVariants } from "@/components/ui/button"; //this is a shadcn component!
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { continueAsGuest } from "./utils/authUtil.js"; // Import from new file
@@ -26,16 +25,16 @@ export default function Home() {
 
         <div className="flex gap-10 -ml-12">
           <div className="w-60">
-            <Link href="/auth/login" className={`${buttonVariants({ variant: "main" })} block`}>Login</Link>
+            <Link href="/auth/login" className="w-full bg-white border border-orange-500 text-orange-500 px-4 py-2 rounded font-semibold hover:bg-orange-500 hover:text-white hover:shadow-md transition text-center block">Login</Link>
           </div>
           <div className="w-60">
-            <Link href="/auth/register" className={`${buttonVariants({ variant: "main" })} block`}>Create Account</Link>
+            <Link href="/auth/register" className="w-full bg-white border border-orange-500 text-orange-500 px-4 py-2 rounded font-semibold hover:bg-orange-500 hover:text-white hover:shadow-md transition text-center block">Create Account</Link>
           </div>
         </div>
         <div className="flex mt-4 -ml-12 w-60">
           <button
             onClick={() => continueAsGuest(router)}
-            className={buttonVariants({ variant: "main" })}
+            className="w-full bg-white border border-orange-500 text-orange-500 px-4 py-2 rounded font-semibold hover:bg-orange-500 hover:text-white hover:shadow-md transition text-center"
           >
             Continue As Guest
           </button>
